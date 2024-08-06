9290536f055f84baa0904d02406abbd4f89ecf1b
OzBirdog <ozbirdett@gmail.com>
2024-08-05 22:44:13 -0500

    resume stuff

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -7,17 +7,19 @@ export const Resume = () => {
         <p className="resume-header">Education</p>
             <div className="resume-box">
                 <div className="resume-box-left">
-                    <p className="position">Help Desk Support</p>
-                    <p className="date">July 2023 - Present</p>
-                    <p className ="text">EMP Manufacturing</p>
-                    <p className ="text">Coppell, TX</p>
+                    <p className="position">B.S. in Computer Science</p>
+                    <p className="position">GPA: 3.532</p>
+                    <p className="date">University of North Texas</p>
+                    <p className="date">ABET Accredited</p>
+                    <p className ="text">2020 - 2024</p>
+                    <p className ="text">Denton, TX</p>
                 </div>    
+                <div className="resume-box-right">
+                    <p className ="paragraph">I attended the Univeristy of North Texas from August of 2020 to May of 2024, where I received a Bachelor's Degree in Computer Science.</p>
+                </div>
             </div>
         <p className="resume-header">Experience</p>
-            <div className="resume-box">
-                <div className="resume-box-left">
-
-                </div>    
+            <div className="resume-box">    
             </div>
     </div>
   )
