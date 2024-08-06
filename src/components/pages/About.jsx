13d6ddeadf9309ed6a9074d21dbf7e320912fb14
OzBirdog <ozbirdett@gmail.com>
2024-08-05 21:44:36 -0500

    pic stuff

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
     <div className='about-container'>
         <div className="about-box">
                     <div className="box-left">
-                        <img src="/personal-website/src/assets/image.jpeg" alt="image" className="box-image" />
+                        <img src="/personal-website/public/assets/image.jpeg" alt="image" className="box-image" />
                         <div className="box-left-title">
                             <p>Oz Birdett</p>
                         </div>
