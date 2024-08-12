64d4916d65e484f45a0da97c76dc552766c13dbd
OzBirdog <ozbirdett@gmail.com>
2024-08-12 15:52:12 -0500

    Working on resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -4,7 +4,7 @@ import './Resume.css'
 export const Resume = () => {
   return (
     <div className="container">
-        <div className="resume-header">Education</div>
+        <div className="resume-header3">Education</div>
             <div className="resume-box">
                 <div className="resume-box-left">
                     <div className="spacing"></div>
@@ -19,6 +19,51 @@ export const Resume = () => {
                     <p className ="paragraph">While attending the University of North Texas I learned about different coding languages, including C++, SQL, JavaScript, HTML, and CSS. I also learned different coding methodologies, like Scrum, Kanban, and Waterfall, while working on various projects. </p>
                 </div>
             </div>
+        <div className="resume-header2">Skills</div>
+            <div className="resume-box">
+                <div className="resume-box-left">
+                    <div className="spacing"></div>
+                    <p className="position">Programming Languages:</p>
+                    <ul className="a">
+                        <li>C/C++ (Interm.)</li>
+                        <li>JavaScript (Interm.)</li>
+                        <li>HTML (Interm.)</li>
+                        <li>CSS (Interm.)</li>
+                        <li>Java (Interm.)</li>
+                        <li>Python (Interm.)</li>
+                        <li>SQL (Beg.)</li>
+                    </ul>
+                    <div className="spacing"></div>
+                    <p className="position">Frameworks:</p>
+                    <ul className="a">
+                        <li>React.js (Interm.)</li>
+                        <li>Streamlit (Interm.)</li>
+                        <li>Flask (Beg.)</li>
+                    </ul>
+                    <div className="spacing3"></div>
+                </div>
+                <div className="resume-box-right">
+                    <div className="spacing"></div>
+                    <p className="position">Software:</p>
+                    <ul className="a">
+                        <li>Visual Studio</li>
+                        <li>Putty</li>
+                        <li>MobaXTerm</li>
+                        <li>Microsoft Office Suit</li>
+                        <li>Microsoft Admin Center</li>
+                        <li>Microsoft Active Directory</li>
+                        <li>Microsoft Azure AD</li>
+                    </ul>
+                    <div className="spacing"></div>
+                    <p className="position">Operating Systems:</p>
+                    <ul className="a">
+                        <li>Windows</li>
+                        <li>MacOS</li>
+                    </ul>
+                    <div className="spacing"></div>
+                    <div className="spacing2"></div>
+                </div>
+           </div>
         <div className="resume-header">Experience</div>
             <div className="resume-box">
                 <div className="resume-box-left">
@@ -43,6 +88,33 @@ export const Resume = () => {
                 </div>
                 <div className="resume-box-right">
                     <p className="paragraph">As a Brand Ambassador, I would represent Honda at community and volunteer invents across the Dallas-Fort Worth Metroplex. At these events I would interact with the public by handing out merchandise and taking photos with them, with the ultimate goal of improving Honda's image.</p>
+                </div>
+            </div>
+            <div className="resume-header"></div>
+            <div className="resume-box">
+                <div className="resume-box-left">
+                    <div className="spacing"></div>
+                    <p className="position">Seasonal Operations Assistant</p>
+                    <p className="date">American Campus Communities</p>
+                    <p className="text">Jul 2022 - Aug 2022,</p>
+                    <p className="text">Jul 2023 - Aug 2023</p>
+                    <p className="text">Denton, TX</p>
+                </div>
+                <div className="resume-box-right">
+                    <p className="paragraph">Assisted in preparing the apartments for new residents by looking for broken or missing items and either fixing or repairing them. Once residents started moving in, I would resolve any maintance issues they had.   </p>
+                </div>
+            </div>
+            <div className="resume-header"></div>
+            <div className="resume-box">
+                <div className="resume-box-left">
+                    <div className="spacing"></div>
+                    <p className="position">Player Assistant</p>
+                    <p className="date">Whitestone Golf Club</p>
+                    <p className="text">Mar 2019 - Jun 2021</p>
+                    <p className="text">Benbrook, TX</p>
+                </div>
+                <div className="resume-box-right">
+                    <p className="paragraph">Started out working outside as an outside lead, where I had to maintain 80 golf carts throughout the day. A year later I was promoted to the Pro-Shop, which included customer service, running the register, managing the outside workers, and closing at the end of the day. </p>
                 </div>
             </div>
     </div>
