386639d1bf96ee74e82eb3827a2a229983467ccb
OzBirdog <ozbirdett@gmail.com>
2024-08-12 16:06:22 -0500

    Added navbar open close

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,11 @@ import "./Navbar.css"
 
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
+
+    const handleLinkClick = () => {
+        setMenuOpen(false);
+    }
+
     return <nav>
         <Link to="/" className="title">Oz Birdett</Link>
         <div className="menu" onClick={() => {
@@ -15,16 +20,16 @@ export const Navbar = () => {
         </div>
         <ul className={menuOpen ? "open" : ""}>
             <li>
-                <NavLink to="/about">About Me</NavLink>
+                <NavLink to="/about" onClick={handleLinkClick}>About Me</NavLink>
             </li>
             <li>
-                <NavLink to="/resume">Resume</NavLink>
+                <NavLink to="/resume" onClick={handleLinkClick}>Resume</NavLink>
             </li>
             <li>
-                <NavLink to="/projects">Projects</NavLink>
+                <NavLink to="/projects" onClick={handleLinkClick}>Projects</NavLink>
             </li>
             <li>
-                <NavLink to="/contact">Contact Me</NavLink>
+                <NavLink to="/contact" onClick={handleLinkClick}>Contact Me</NavLink>
             </li>
         </ul>
     </nav>
