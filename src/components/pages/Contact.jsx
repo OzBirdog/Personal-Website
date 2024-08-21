8724567bf7812348bad945fa378669967ebc65c8
OzBirdog <ozbirdett@gmail.com>
2024-08-21 18:58:57 -0500

    Changed email service

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,149 +1,185 @@
-import React, { useState, useEffect } from 'react';
-import { useForm, useWatch } from 'react-hook-form';
-import './Contact.css';
+import { React, useState} from 'react'
+import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
+import './Contact.css'
 
 export const Contact = () => {
   const {
     register,
     handleSubmit,
     reset,
-    setValue,
-    control,
-    formState: { errors, isSubmitting, isSubmitSuccessful },
-  } = useForm({ mode: 'onTouched' });
-
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [message, setMessage] = useState('');
+    formState: { errors },
+  } = useForm();
   const [disabled, setDisabled] = useState(false);
-
-  const userName = useWatch({
-    control,
-    name: 'name',
-    defaultValue: 'Someone',
+  const [alertInfo, setAlertInfo] = useState({
+    display: false,
+    message: '',
+    type: '',
   });
 
-  useEffect(() => {
-    setValue('subject', `${userName} sent a message from Website`);
-  }, [userName, setValue]);
+  const toggleAlert = (message, type) => {
+    setAlertInfo({ display: true, message, type });
 
-  const onSubmit = async (data, e) => {
+    setTimeout(() => {
+      setAlertInfo({ display: false, message: '', type: '' });
+    }, 5000);
+  };
+
+  const onSubmit = async (data) => {
+    const { name, email, subject, message } = data;
     try {
       setDisabled(true);
 
-      const response = await fetch('https://api.web3forms.com/submit', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-        body: JSON.stringify({ ...data, access_key: 'ed1656a5-2642-4e90-b25f-f47e1044e976' }, null, 2),
-      });
+      const templateParams = {
+        name,
+        email,
+        subject,
+        message,
+      };
 
-      const json = await response.json();
+      await emailjs.send(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        templateParams,
+        import.meta.env.VITE_PUBLIC_KEY,
+      );
 
-      if (json.success) {
-        setIsSuccess(true);
-        setMessage(json.message);
-        e.target.reset();
-        reset();
-      } else {
-        setIsSuccess(false);
-        setMessage(json.message);
-      }
-    } catch (error) {
-      setIsSuccess(false);
-      setMessage('Client Error. Please check the console.log for more info');
-      console.error(error);
+      toggleAlert('Email was successfuly sent!', 'success');
+    } catch (e) {
+      console.error(e);
+      toggleAlert('Oops, something went wrong.', 'danger');
     } finally {
       setDisabled(false);
+      reset();
     }
   };
 
   return (
     <div className="contact-container">
       <div className="contact-box">
-        <h1 className="contact-header">Contact Form</h1>
-        <p className="contact-msg">Feel free to reach out!</p>
-          <form id="contact-form" onSubmit={handleSubmit(onSubmit)} noValidate>
-            <input type="hidden" value="ed1656a5-2642-4e90-b25f-f47e1044e976" {...register('access_key')} />
-            <input type="hidden" {...register('subject')} />
-            <input type="hidden" value="Portfolio" {...register('from_name')} />
-
-            <div className="formRow">
-              <input
-                type="text"
-                placeholder="Full Name"
-                autoComplete="false"
-                className={`formInput ${errors.name ? 'border-red-600' : 'border-gray-300'}`}
-                {...register('name', {
-                  required: 'Full name is required',
-                  maxLength: 80,
-                })}
-              />
-              {errors.name && <span className="errorMessage">{errors.name.message}</span>}
-            </div>
-
-            <div className="formRow">
-              <input
-                type="email"
-                placeholder="Email Address"
-                autoComplete="false"
-                className={`formInput ${errors.email ? 'border-red-600' : 'border-gray-300'}`}
-                {...register('email', {
-                  required: 'Enter your email',
-                  pattern: {
-                    value: /^\S+@\S+$/i,
-                    message: 'Please enter a valid email',
-                  },
-                })}
-              />
-              {errors.email && <span className="errorMessage">{errors.email.message}</span>}
-            </div>
-
-            <div className="formRow">
-              <input
-                type="text"
-                placeholder="Subject"
-                autoComplete="false"
-                className={`formInput ${errors.subjectLine ? 'border-red-600' : 'border-gray-300'}`}
-                {...register('subjectLine', {
-                  required: 'Enter your Subject',
-                  maxLength: 100,
-                })}
-              />
-              {errors.subjectLine && <span className="errorMessage">{errors.subjectLine.message}</span>}  
-            </div>
-
-            <div className="formRow">
-              <textarea
-                placeholder="Your Message"
-                className={`formInput ${errors.message ? 'border-red-600' : 'border-gray-300'}`}
-                {...register('message', { required: 'Enter your message' })}
-              />
-              {errors.message && <span className="errorMessage">{errors.message.message}</span>}
-            </div>
-
-            <button
-              className="submit-button"
-              disabled={disabled || isSubmitting}
-              type="submit"
-            >
-              Send Message
-            </button>
-          </form>
-
-          {isSubmitSuccessful && isSuccess && (
-          <div className="alert alert-success">
-            <p>{message}</p>
-          </div>
+      <h1 className="contact-header">Contact Form</h1>
+      <p className="contact-msg">Feel free to reach out!</p>
+        <form
+          id='contact-form'
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
+        <div className="formRow"> 
+          <input
+            type='text'
+            name='name'
+            {...register('name', {
+              required: {
+                value: true,
+                message: 'Please enter your name',
+              },
+              maxLength: {
+                value: 30,
+                message: 'Please use 30 characters or less',
+              },
+            })}
+            className='formInput'
+            placeholder='Name'
+          ></input>
+          {errors.name && (
+            <span className='errorMessage'>
+              {errors.name.message}
+            </span>
           )}
-
-          {isSubmitSuccessful && !isSuccess && (
-            <div className="alert alert-error">
-              <p>Oops, Something went wrong!</p>
-            </div>
+        </div>  
+        <div className="formRow">
+          <input
+            type='email'
+            name='email'
+            {...register('email', {
+              required: true,
+              pattern:
+                 /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            })}
+            className='formInput'
+            placeholder='Email address'
+          ></input>
+          {errors.email && (
+            <span className='errorMessage'>
+                Please enter a valid email address
+              </span>
           )}
+        </div>
+        <div className="formRow">
+        <input
+          type='text'
+          name='subject'
+          {...register('subject', {
+            required: {
+              value: true,
+              message: 'Please enter a subject',
+            },
+          })}
+          className='formInput'
+          placeholder='Subject'
+          ></input>
+          {errors.subject && (
+            <span className='errorMessage'>
+              {errors.subject.message}
+            </span>
+          )}
+        </div>
+        <div className="formRow">
+          <textarea
+            rows={3}
+            name='message'
+            {...register('message', {
+              required: true,
+            })}
+            className='formInput'
+            placeholder='Message'
+          ></textarea>
+          {errors.message && (
+            <span className='errorMessage'>
+              Please enter a message
+            </span>
+          )}
+        </div>
+        <button
+          className="submit-button"
+          disabled={disabled}
+          type='submit'
+        >
+          Submit
+        </button>  
+        </form>
+        {alertInfo.display && (
+        <div
+          className={`alert alert-${alertInfo.type} alert-dismissible mt-5`}
+          role='alert'
+        >
+          {alertInfo.message}
+          <button
+            type='button'
+            className='btn-close'
+            data-bs-dismiss='alert'
+            aria-label='Close'
+            onClick={() =>
+              setAlertInfo({ display: false, message: '', type: '' })
+            }
+          ></button>
+        </div>
+        )}
+      </div>
+      <div className="contact-spacing"></div>
+      <div className="contact-box">
+        <div className="contact-header">Contact Information</div>
+          <p className="contact-list">ozbirdett@gmail.com</p>
+          <p className="contact-list">817-559-5284</p>
+        <div className="contact-header">Links</div>
+          <a className="link" href="https://www.linkedin.com/in/ozbirdett/">
+            <p className="contact-list">LinkedIn</p>
+          </a>
+          <a className="link" href="https://github.com/OzBirdog">
+            <p className="contact-list">Github</p>
+          </a>
+
       </div>
     </div>
-  );
-};
+  )
+}
