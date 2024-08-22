153fb6e847e846a1417f85e85377cc118f0b85c7
Birdett, Oz <ozbirdett@my.unt.edu>
2024-08-22 15:28:09 -0500

    margin fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 import { Navbar } from './components/Navbar'
 import { About } from './components/pages/About'
 import { Contact } from './components/pages/Contact'
-import { Home } from './components/pages/Home'
 import { Projects } from './components/pages/Projects'
 import { Resume } from './components/pages/Resume'
 
