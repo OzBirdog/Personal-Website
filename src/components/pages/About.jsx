40b93204baa8be0a98f57550811f5a088cdac218
Birdett, Oz <ozbirdett@my.unt.edu>
2025-04-15 01:10:37 -0500

    Fixed mobile version

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -30,8 +30,8 @@ export const About = () => {
                             <Button text="Resume" className="button-resume" link="/resume"/>
                             <Button text="Projects" style={{ backgroundColor: '#2196F3', color: 'white' }} className="button-projects" link="/projects"/>
                         </div>
-                        <p className="info-mobile">My name is Oz Birdett and I graduated from the University of North Texas with a bachelor’s degree in Computer Science. What I am most passionate about is solving problems, which I’ve loved to do ever since I was a little kid building Legos.</p>
-                        <p className="info-mobile">Now the Legos have been replaced with coding, but it’s just as fun and rewarding. Over the years I’ve been able to grow my problem solving skills by taking coding classes and working jobs that require quick solutions. I hope to further utilize and grow my passion for problem solving at my next career.</p>
+                        <p className="info-mobile">My name is Oz Birdett and I graduated from the University of North Texas with a bachelor’s degree in Computer Science. I've always been passionate about solving problems, never being able to get enough of the satisfaction that comes from a solution.</p>
+                        <p className="info-mobile">Over the years I’ve been able to find that satisfaction and grow my problem solving skills from coding and providing technical support. I hope to be able to continue my passion for problem solving at my next career.</p>
                     </div>
                 </div>
     </div>
